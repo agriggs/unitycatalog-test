eca4ffb57a83f43ae992d939eb9e271b95d84812
agriggs <agriggs@mitre.org>
2025-05-12 13:04:15 -0700

    Testing from Windows Spark to WSL (catalog)

--- spark_start.sh
+++ spark_start.sh
@@ -1,5 +1,5 @@
 pyspark --name "local-uc-test" \
-    --master "local[*]" \
+    --master "spark://localhost" \
     --packages "io.delta:delta-spark_2.12:3.2.1,io.unitycatalog:unitycatalog-spark_2.12:0.2.0" \
     --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
     --conf "spark.sql.catalog.spark_catalog=io.unitycatalog.spark.UCSingleCatalog" \
