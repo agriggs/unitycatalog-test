964cbec7a22e25708d1f1b0ef2e1242499043e6c
agriggs <agriggs@mitre.org>
2025-05-19 12:04:00 -0700

    Moved Databrick CLI OAuth to common code

--- unity_catalog_databricks.py
+++ unity_catalog_databricks.py
@@ -1,53 +1,25 @@
-import subprocess
-import json
-import logging
+from config import *
 
-from _env import *
 from spark_config import get_spark_session
-
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-logger = logging.getLogger(__name__)
+from databricks_cli import get_databricks_token
 
 # Configuration
 databricks_profile = databricks_cli_profile
-workspace_url = "https://adb-3947916102150554.14.azuredatabricks.net"
 uc_api_path = "api/2.1/unity-catalog"
 uc_catalog_name = databricks_catalog
 uc_catalog_url = databricks_unity_catalog_url
 schema_name = "govcontracting"
 table_name = "cancelled_awards"
 
-def get_databricks_token():
-    logger.info(f"Get Databricks OAuth token using CLI for profile: {databricks_profile}")
-    try:
-        result = subprocess.run(
-            ['databricks', 'auth', 'token', '--profile', databricks_profile, '--output', 'JSON'],
-            capture_output=True,
-            text=True
-        )
-        if result.returncode != 0:
-            raise Exception(f"Error retrieving Databricks OAuth token: {result.stderr}")
-       
-        token_data = json.loads(result.stdout)
-        if 'access_token' in token_data:
-            logger.info("Databricks OAuth token retrieved successfully.")
-            return token_data['access_token']
-        else:
-            raise ValueError("Access token not found in response")
-    except Exception as e:
-        raise Exception(f"Failed to retrieve Databricks OAuth token: {str(e)}")
-
 def main():
     try:
         # Get Databricks token and set up headers
         # Doesn't work so using PAT
-        token = get_databricks_token()
-        # token = databricks_unity_catalog_admin_token
-
+        if databricks_unity_catalog_admin_token:
+            token = databricks_unity_catalog_admin_token
+        else:
+            token = get_databricks_token(databricks_profile)
+    
         logger.info(f"Create Spark session")
         spark = get_spark_session(uc_catalog_url, uc_catalog_name, token)
 
